71747dadb86db2929cbc54bb5b046e115b1b522f
root <root@LAPTOP-UBKTGC3S.>
2024-11-25 01:00:08 +0800

    Initial commit of dist folder

---
+++ js/data/sets/gen9.js
@@ -0,0 +1,179 @@
+var SETDEX_SV = {
+    "Ditto": {
+        "Youngster Calvin": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Imposter",
+            "nature": "Bold",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Transform"
+            ],
+            "index": 0
+        }
+    },
+    "Yanma": {
+        "Bug Catchter Rick": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Speed Boost",
+            "nature": "Hardy",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Sonic Boom",
+                "Bug Bite",
+                "Aerial Ace"
+            ],
+            "index": 1
+        }
+    },
+    "Rellor": {
+        "Bug Catchter Rick": {
+            "level": 13,
+            "ivs": null,
+            "item": "Berry Juice",
+            "ability": "Shed Skin",
+            "nature": "Careful",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Sand Attack",
+                "Defend Curl",
+                "Rollout"
+            ],
+            "index": 2
+        }
+    },
+    "Nincada": {
+        "Bug Catchter Rick": {
+            "level": 13,
+            "ivs": null,
+            "item": "Muscle Band",
+            "ability": "Compound Eyes",
+            "nature": "Brave",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Dig",
+                "Bug Bite",
+                "Fury Swipes"
+            ],
+            "index": 3
+        }
+    },
+    "Chikorita": {
+        "Youngster Allen": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Overgrow",
+            "nature": "Hardy",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Giga Drain",
+                "Synthesis"
+            ],
+            "index": 4
+        }
+    },
+    "Bunnelby": {
+        "Youngster Allen": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Cheek Pouch",
+            "nature": "Impish",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Flail",
+                "Bulk Up"
+            ],
+            "index": 5
+        }
+    },
+    "Wingull": {
+        "Girl Shaw": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Hydration",
+            "nature": "Quite",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Water Gun",
+                "Air Cutter"
+            ],
+            "index": 6
+        }
+    },
+    "Psyduck": {
+        "Girl Shaw": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Damp",
+            "nature": "Hardy",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Metronome"
+            ],
+            "index": 7
+        }
+    },
+    "Chewtle": {
+        "Fisherman Darian": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Shell Armor",
+            "nature": "Hardy",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Bite",
+                "Dive"
+            ],
+            "index": 8
+        }
+    },
+    "Luvdisc": {
+        "Fisherman Darian": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Hydration",
+            "nature": "Hardy",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Water Pulse",
+                "Agility"
+            ],
+            "index": 9
+        }
+    },
+    "Goldeen": {
+        "Fisherman Darian": {
+            "level": 12,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Swift Swim",
+            "nature": "Adamant",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Horn Attack",
+                "Dive"
+            ],
+            "index": 10
+        }
+    }
+}
