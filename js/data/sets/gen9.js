7c8392591b73e5cec593e11b703a09e42f4506b8
root <root@LAPTOP-UBKTGC3S.>
2025-03-24 21:46:49 +0800

    Route 3

--- js/data/sets/gen9.js
+++ js/data/sets/gen9.js
@@ -384,5 +384,350 @@ var SETDEX_SV = {
             ],
             "index": 21
         }
-    }
+    },
+
+    "Hattrem": {
+        "Lass Janice ": {
+            "level": 25,
+            "ivs": null,
+            "item": "Wise Glasses",
+            "ability": "Magic Bounce",
+            "nature": "Modest",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Draining Kiss",
+                "Psybeam",
+                "Thunder Wave",
+                "Life Dew"
+            ],
+            "index": 22
+        }
+    },
+
+    "Audino": {
+        "Lass Janice ": {
+            "level": 25,
+            "ivs": null,
+            "item": "Oran Berry",
+            "ability": "Regenerator",
+            "nature": "Bashful",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Swagger",
+                "Wish",
+                "Protect",
+                "Toxic"
+            ],
+            "index": 23
+        }
+    },
+    
+    "Lowkix": {
+        "Bug Catcher Colton ": {
+            "level": 25,
+            "ivs": null,
+            "item": "Berry Juice",
+            "ability": "Swarm",
+            "nature": "Adamant",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Bug Bite",
+                "Assurance",
+                "Low Kick",
+                "Scary Face"
+            ],
+            "index": 24
+        }
+    },
+    
+    "Ledian": {
+        "Bug Catcher Colton ": {
+            "level": 25,
+            "ivs": null,
+            "item": "Lum Berry",
+            "ability": "Swarm",
+            "nature": "Admant",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Drain Punch",
+                "Thunder Punch",
+                "Ice Punch",
+                "Mach Punch"
+            ],
+            "index": 25
+        }
+    },
+    
+    "Durant": {
+        "Bug Catcher Colton ": {
+            "level": 25,
+            "ivs": null,
+            "item": "Muscle Band",
+            "ability": "Iron Fist",
+            "nature": "Jolly",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "First Impression",
+                "Iron Head",
+                "X-Scissor",
+                "Thunder Fang"
+            ],
+            "index": 26
+        }
+    },
+
+    "Raticate": {
+        "Youngster Ben": {
+            "level": 25,
+            "ivs": null,
+            "item": "Flame Orb",
+            "ability": "Guts",
+            "nature": "Jolly",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Hyper Fang",
+                "Zen Headbutt",
+                "Protect",
+                "Sucker Punch"
+            ],
+            "index": 27
+        }
+    },
+
+    "Squawkabilly": {
+        "Youngster Ben": {
+            "level": 25,
+            "ivs": null,
+            "item": "Muscle Band",
+            "ability": "Hustle",
+            "nature": "Jolly",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Aerial Ace",
+                "Take Down",
+                "Reversal",
+                "U-turn"
+            ],
+            "index": 28
+        }
+    },
+    
+    "Parasect": {
+        "Bug Catcher Greg": {
+            "level": 25,
+            "ivs": null,
+            "item": "Focus Sash",
+            "ability": "Effect Spore",
+            "nature": "Adamant",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Leech Life",
+                "Seed Bomb",
+                "Spore",
+                "Light Screen"
+            ],
+            "index": 29
+        }
+    },
+
+    "Yanma": {
+        "Bug Catcher Greg": {
+            "level": 25,
+            "ivs": null,
+            "item": "Berry Juice",
+            "ability": "Speed Boost",
+            "nature": "Adamant",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Leech Life",
+                "Wing Attack",
+                "Giga Drain",
+                "Sweet Kiss"
+            ],
+            "index": 30
+        }
+    },
+
+    "Volbeat": {
+        "Bug Catcher Greg": {
+            "level": 25,
+            "ivs": null,
+            "item": "Expert Belt",
+            "ability": "Prankster",
+            "nature": "Jolly",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Thunder Punch",
+                "Ice Punch",
+                "Aerial Ace",
+                "Moonlight"
+            ],
+            "index": 31
+        }
+    },    
+
+    "Illumise": {
+        "Bug Catcher Greg": {
+            "level": 25,
+            "ivs": null,
+            "item": "Wise Glasses",
+            "ability": "Prankster",
+            "nature": "Timid",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Dazzling Gleam",
+                "Struggle Bug",
+                "Water Pulse",
+                "Moonlight"
+            ],
+            "index": 32
+        }
+    },  
+
+    "Granbull": {
+        "Lass Sally": {
+            "level": 25,
+            "ivs": null,
+            "item": "Berry Juice",
+            "ability": "Intimidate",
+            "nature": "Careful",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Play Rough",
+                "Brick Break",
+                "Crunch",
+                "Bulk Up"
+            ],
+            "index": 33
+        }
+    },  
+
+    "Togetic": {
+        "Lass Sally": {
+            "level": 25,
+            "ivs": null,
+            "item": "Eviolite",
+            "ability": "Serene Grace",
+            "nature": "Bashful",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Draining Kiss",
+                "Thunder Wave",
+                "Ancient Power",
+                "Mystical Fire"
+            ],
+            "index": 34
+        }
+    },  
+
+    "Heracross": {
+        "Bug Catcher James": {
+            "level": 25,
+            "ivs": null,
+            "item": "Coba Berry",
+            "ability": "Swarm",
+            "nature": "Adamant",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Protect",
+                "Rock Tomb",
+                "Rock Smash",
+                "Pounce"
+            ],
+            "index": 35
+        }
+    },  
+
+    "Ariados": {
+        "Bug Catcher James": {
+            "level": 25,
+            "ivs": null,
+            "item": "Lum Berry",
+            "ability": "Sniper",
+            "nature": "Adamant",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Poison Jab",
+                "Leech Life",
+                "Stomping Tantrum",
+                "Shadow Sneak"
+            ],
+            "index": 36
+        }
+    },  
+
+    "Whimsicott": {
+        "Lass Robin": {
+            "level": 26,
+            "ivs": {
+                "at": 30,
+                "sp": 30
+            },
+            "item": "Leftovers",
+            "ability": "Prankster",
+            "nature": "Modest",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Leech Seed",
+                "Giga Drain",
+                "Hidden Power Psychic",
+                "Dazzling Gleam"
+            ],
+            "index": 37
+        }
+    },  
+
+    "Noctowl": {
+        "Lass Robin": {
+            "level": 26,
+            "ivs": null,
+            "item": "Wide Lens",
+            "ability": "Insomnia",
+            "nature": "Timid",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Moonblast",
+                "Air Slash",
+                "Dream Eater",
+                "Hypnosis"
+            ],
+            "index": 38
+        }
+    },  
+
+    "Simipour": {
+        "Lass Robin": {
+            "level": 26,
+            "ivs": null,
+            "item": "Mystic Water",
+            "ability": "Torrent",
+            "nature": "Timid",
+            "teraType": null,
+            "status": "Healthy",
+            "moves": [
+                "Water Pledge",
+                "Brine",
+                "Icy Wind",
+                "Grass Knot"
+            ],
+            "index": 39
+        }
+    }  
 }
